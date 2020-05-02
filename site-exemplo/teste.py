9853aef282a33426bef3c56ee33e3f2e76f244dc
Hudson Ferreira <hudsonnick12@outlook.com>
2020-05-02 14:54:06 -0300

    Delete teste.py

--- site-exemplo/teste.py
+++
@@ -1,7 +0,0 @@
-import seaborn as sns
-import pandas as pds 
-import matplotlib.pyplot as plt
-import os
-
-notas = pds.read_csv('C:/Users/geofh/Desktop/PROJETO END TO END 1 PYTHON/Alura/ml-latest-small/ratings.csv')
-notas.columns
